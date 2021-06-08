7e5ea4f86c1415eb66558bf44f613e21a3c288cd
YaroslavStynskyi <stynskyi.yaroslav@pdffiller.team>
2021-06-08 14:46:17 +0300

    Fix array response type

--- src/EntityManager/Annotation/ResponseType.php
+++ src/EntityManager/Annotation/ResponseType.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace SignNow\Rest\EntityManager\Annotation;
 
 use InvalidArgumentException;
+use SignNow\Rest\Entity\Binary;
 
 /**
  * Class ResponseType
@@ -40,8 +41,11 @@ class ResponseType extends Annotation
     {
         if ($this->type === self::DEFAULT_TYPE) {
             $this->type = $this->owner->getEntityClass();
+        } else if (preg_match("#(array<)?(?<type>[a-zA-Z0-9_\\\]+)>?#i", $this->type, $matches)) {
+            $this->type = $matches['type'];
         }
         
+        $this->type = trim($this->type, '\\');
         if (!class_exists($this->type)) {
             throw new InvalidArgumentException('Response type ['.$this->type.'] does not exists');
         }
@@ -53,5 +57,13 @@ class ResponseType extends Annotation
     public function getType(): string
     {
         return $this->type;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function isBinary(): bool
+    {
+        return $this->type == Binary::class;
     }
 }
